869d91a77db032067eb06fab88cb1e0ac392d0e2
Roman <68429513+SayHelloRoman@users.noreply.github.com>
2021-04-12 23:57:13 +0300

    Add files via upload

---
+++ DB.py
@@ -0,0 +1,29 @@
+from typing import Optional
+import asyncio
+import asyncpg
+import json
+
+class DataBase:
+    async def init(self):
+        with open("config.json") as f:
+            self.conn = await asyncpg.connect(
+                **json.load(f)
+            )
+
+    async def create(self, guild_id: Optional[int], channel_id: Optional[bool]):
+        await self.conn.execute(
+            f'''INSERT INTO guilds(guild_id, channel_id) VALUES({guild_id}, {channel_id})'''
+        )
+    
+    async def update(self, guild_id: Optional[int], channel_id: Optional[bool]):
+        await self.conn.execute(f"""
+        UPDATE guilds SET 
+        channel_id = {channel_id}
+        WHERE guild_id = {guild_id}
+        """)
+    
+    async def search(self, guild_id: Optional[int]):
+        return await self.conn.fetchrow(f"SELECT * FROM guilds WHERE guild_id = {guild_id}")
+    
+    async def get_ids(self):
+        return await self.conn.fetch(f"SELECT * FROM guilds")
